68f9e6a14ce345d92ca5e71f6f0525bc99f3252e
Maxwell Croy <max.croy1@gmail.com>
2020-05-29 16:46:42 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -28,6 +28,8 @@ def consolidate_cart(cart)
         item_incremented = true
       end
       if item_incremented = false
+        new_item = cart[i]
+        new_item[:count] = 1
         consolidated_cart << cart[i]
         
       end

5afc28241afa54f7b8010260c5735615a3bf9c43
Maxwell Croy <max.croy1@gmail.com>
2020-05-29 16:43:03 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,7 +20,7 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   consolidated_cart = [cart.first]
   consolidated_cart[0][:count] = 1
-  for i in 0...(consolidated_cart.length)
+  for i in 1...(cart.length)
     for y in 1...(cart.length)
       if cart[y][:item] == consolidated_cart[i][:item]
         consolidated_cart[i][:count] += 1

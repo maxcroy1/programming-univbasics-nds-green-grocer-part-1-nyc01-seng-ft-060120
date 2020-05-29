a7d29a8dcaa35c5e8eb8ef98a797aefe4431e240
Maxwell Croy <max.croy1@gmail.com>
2020-05-29 16:16:57 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,6 +20,7 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   consolidated_cart = []
   if consolidated_cart.length < 0 
+    
   end
   cart.each do |item|
     for i in 0...(consolidated_cart.length)

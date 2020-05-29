4d5b47d8a9ddc033e4121233962e03d0c36ca96d
Maxwell Croy <max.croy1@gmail.com>
2020-05-29 16:30:46 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,12 +19,11 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   consolidated_cart = []
-  cart.each do |item|
-    item_count = 0
-    increment_variable = 0
-    while increment_variable <= consolidated_cart.length do
-      if consolidated_cart[increment_variable][:item]
-    end
+  consolidated_cart << cart.firt
+  item_count = 0
+  increment_variable = 0
+  while increment_variable <= consolidated_cart.length do
+    if consolidated_cart[increment_variable][:item]
   end
   consolidated_cart
 end

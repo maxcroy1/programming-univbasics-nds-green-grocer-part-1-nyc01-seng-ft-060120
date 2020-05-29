10b939202a61b465cf9f44f7550a2de9c14651b8
Maxwell Croy <max.croy1@gmail.com>
2020-05-29 16:44:02 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -24,7 +24,7 @@ def consolidate_cart(cart)
   for i in 1...(cart.length)
     for y in 0...(consolidated_cart.length)
       if cart[i][:item] == consolidated_cart[y][:item]
-        consolidated_cart[i][:count] += 1
+        consolidated_cart[y][:count] += 1
       end
     end
   end
